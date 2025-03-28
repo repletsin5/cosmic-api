d8afe666337bb218a12fd1fa4ca2b1e5d3f0f9fc
Crab_King <127689590+Crab-K1ng@users.noreply.github.com>
2025-03-28 22:00:12 +0000

    add basicTool and BasicItem

---
+++ src/commonBackend/java/io/github/puzzle/cosmic/item/BasicItem.java
@@ -0,0 +1,47 @@
+package io.github.puzzle.cosmic.item;
+
+import finalforeach.cosmicreach.util.Identifier;
+
+import static io.github.puzzle.cosmic.CosmicConstants.MOD_ID;
+
+public class BasicItem extends AbstractCosmicItem {
+
+    String name;
+
+    public BasicItem(Identifier id) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(ItemModelType.ITEM_MODEL_3D));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(Identifier.of(id.getNamespace(), "textures/items/" + id.getName() + ".png")));
+    }
+
+    public BasicItem(Identifier id, ItemModelType itemModelType) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(itemModelType));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(Identifier.of(id.getNamespace(), "textures/items/" + id.getName() + ".png")));
+    }
+
+    public BasicItem(Identifier id, Identifier textureLocation) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(ItemModelType.ITEM_MODEL_3D));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(textureLocation));
+    }
+
+    public BasicItem(Identifier id, Identifier textureLocation, ItemModelType itemModelType) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(itemModelType));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(textureLocation));
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/commonBackend/java/io/github/puzzle/cosmic/item/BasicTool.java
@@ -0,0 +1,55 @@
+package io.github.puzzle.cosmic.item;
+
+import finalforeach.cosmicreach.util.Identifier;
+
+public class BasicTool extends AbstractCosmicItem {
+
+    String name;
+
+    public BasicTool(Identifier id) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(ItemModelType.ITEM_MODEL_3D));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(Identifier.of(id.getNamespace(), "textures/items/" + id.getName() + ".png")));
+    }
+
+    public BasicTool(Identifier id, ItemModelType itemModelType) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(itemModelType));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(Identifier.of(id.getNamespace(), "textures/items/" + id.getName() + ".png")));
+    }
+
+    public BasicTool(Identifier id, Identifier textureLocation) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(ItemModelType.ITEM_MODEL_3D));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(textureLocation));
+    }
+
+    public BasicTool(Identifier id, Identifier textureLocation, ItemModelType itemModelType) {
+        super(id);
+        name = id.getName();
+        manifest.put(ItemDataPointSpecs.MODEL_TYPE.create(itemModelType));
+        manifest.put(ItemDataPointSpecs.TEXTURE_LOCATION.create(textureLocation));
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean pIsTool() {
+        return true;
+    }
+
+    @Override
+    public int getDefaultStackLimit() {
+        return 1;
+    }
+}
