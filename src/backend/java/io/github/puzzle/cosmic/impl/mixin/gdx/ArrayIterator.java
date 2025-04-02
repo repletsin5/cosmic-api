ff724c5094418b15195d9508b6ef01272cbee8a7
Mr-Zombii <thezombiigames@gmail.com>
2025-04-02 18:24:44 -0500

    kill array iterator bug

---
+++ src/backend/java/io/github/puzzle/cosmic/impl/mixin/gdx/ArrayIterator.java
@@ -0,0 +1,41 @@
+package io.github.puzzle.cosmic.impl.mixin.gdx;
+
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.NoSuchElementException;
+
+@Mixin(Array.ArrayIterator.class)
+public class ArrayIterator<T> {
+
+    @Shadow(remap = false) private int index;
+
+    @Shadow(remap = false) @Final private Array<T> array;
+
+    /**
+     * @author Mr_Zombii
+     * @reason Fuck you libgdx
+     */
+    @Overwrite(remap = false)
+    public boolean hasNext() {
+        return this.index < this.array.size;
+    }
+
+    /**
+     * @author Mr_Zombii
+     * @reason Fuck you libgdx
+     */
+    @Overwrite(remap = false)
+    public T next() {
+        if (this.index >= this.array.size) {
+            throw new NoSuchElementException(String.valueOf(this.index));
+        } else {
+            return this.array.items[this.index++];
+        }
+    }
+
+}
